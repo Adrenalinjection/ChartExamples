faba1533304b164df3f2d614c503d6ea5ca1717f
Maria Klichko <65122060+Adrenalinjection@users.noreply.github.com>
2022-05-08 15:47:00 +0300

    Добавление гистограммы Seaborn

--- ChartExamples.py
+++ ChartExamples.py
@@ -6,6 +6,7 @@ import tkinter as tk
 import chart1
 import chart2
 import chart3
+import chart4
 
 # Функция закрытия программы
 def do_close():
@@ -49,10 +50,10 @@ lblChart4 = tk.Label(text = "Нормальное распределение - 3
 lblChart4.place(x=170, y=272)
 
 # Добавление кнопки и метки для графика 5
-btnChart5 = tk.Button(window, text = "График 5", font = ('Helvetica', 10, 'bold'), command=chart3.plot_chart)
+btnChart5 = tk.Button(window, text = "График 5", font = ('Helvetica', 10, 'bold'), command=chart4.plot_chart)
 btnChart5.place(x=40, y=315, width=90, height=30)
 
-lblChart5 = tk.Label(text = "Описание графика")
+lblChart5 = tk.Label(text = "Гистограмма Seaborn")
 lblChart5.place(x=170, y=322)
 
 # Добавление кнопки и метки для графика 6
